fd92ca5087d4e0c2dcb9f40d5c36502eca2871fa
Yunze6666666 <2749615980@qq.com>
2026-08-08 11:50:09 +0800

    feat:完成了简单的匿名内部类代码练习

--- src/com/demo/anonymousInnerClassDemo/Main.java
+++ src/com/demo/anonymousInnerClassDemo/Main.java
@@ -13,10 +13,10 @@ public class Main {
         };
         /*
         new Calculator()：Calculator 是接口，接口本身不能 new 对象！
-这里不是实例化接口，大括号{}代表创建一个 Calculator 的匿名实现子类，new 的是这个看不见名字的子类对象。
-{ @Override void calculate() {...} }：匿名子类的类体，必须重写接口全部抽象方法。
-把这个匿名子类的对象赋值给父接口引用Calculator add1 。
-分号;不能漏掉，完整是一条语句。
+        这里不是实例化接口，大括号{}代表创建一个 Calculator 的匿名实现子类，new 的是这个看不见名字的子类对象。
+        { @Override void calculate() {...} }：匿名子类的类体，必须重写接口全部抽象方法。
+        把这个匿名子类的对象赋值给父接口引用Calculator add1 。
+        分号;不能漏掉，完整是一条语句。
          */
 
         Calculator reduce1=new Calculator(){
@@ -27,7 +27,7 @@ public class Main {
         };
         /*
         new Calculator() { ... } 的真实含义是：
-"我要创建一个类，它实现了 Calculator接口，然后立刻 new 一个它的实例。
+        "我要创建一个类，它实现了 Calculator接口，然后立刻 new 一个它的实例。
          */
 
         Scanner sc=new Scanner(System.in);
